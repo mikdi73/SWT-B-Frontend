a1b38f1ea4585dbe3aad3bc38d25a5c71730cd9a
mikdi73 <github@mikediefenbach.de>
2025-04-29 14:45:34 +0200

    fix: routing, styling

--- Kidsapp/src/App.tsx
+++ Kidsapp/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css"
+import { Routes, Route } from "react-router"
+import Homepage from "./pages/Homepage.tsx"
+import Favoritenansicht from "./pages/Favoritenansicht.tsx"
+import Detailansicht from "./pages/Detailansicht.tsx"
+import Kartenansicht from "./pages/Kartenansicht.tsx"
+import Anmelden from "./pages/Anmelden.tsx"
+import Regristrieren from "./pages/Regristrieren.tsx"
+import Userprofil from "./pages/Userprofil.tsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/homepage" element={<Homepage />} />
+                <Route path="/favoriten" element={<Favoritenansicht />} />
+                <Route path="/details" element={<Detailansicht />} />
+                <Route path="/karte" element={<Kartenansicht />} />
+                <Route path="/anmelden" element={<Anmelden />} />
+                <Route path="/regristrieren" element={<Regristrieren />} />
+                <Route path="/user" element={<Userprofil />} />
+            </Routes>
+        </>
+    )
 }
 
 export default App
--- Kidsapp/src/main.tsx
+++ Kidsapp/src/main.tsx
@@ -1,26 +1,27 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
-import './index.css'
-import App from './App.tsx'
-import Homepage from "./pages/Homepage.tsx"
-import Favoritenansicht from "./pages/Favoritenansicht.tsx"
-import Detailansicht from "./pages/Detailansicht.tsx"
-import Kartenansicht from "./pages/Kartenansicht.tsx"
-import Anmelden from "./pages/Anmelden.tsx"
-import Regristrieren from "./pages/Regristrieren.tsx"
-import Userprofil from "./pages/Userprofil.tsx"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import { StrictMode } from "react"
+import { BrowserRouter } from "react-router"
+import NavbarTop from "./components/navbarTop.tsx"
+import NavbarBottom from "./components/navbarBottom.tsx"
 
-createRoot(document.getElementById('root')!).render(
- <BrowserRouter>
-     <Routes>
-         <Route path="/" element={<Homepage/>}/>
-         <Route path="/test" element={<App/>}/>
-         <Route path="/favoriten" element={<Favoritenansicht/>}/>
-         <Route path="/details" element={<Detailansicht/>}/>
-         <Route path="/kartenansicht" element={<Kartenansicht/>}/>
-         <Route path="/anmelden" element={<Anmelden/>}/>
-         <Route path="/regristrieren" element={<Regristrieren/>}/>
-         <Route path="/user" element={<Userprofil/>}/>
-     </Routes>
- </BrowserRouter>
-)
+const rootElement = document.getElementById("root")
+
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <StrictMode>
+            {/*TODO implement AuthGate */}
+            {/* <AuthGate> */}
+            <BrowserRouter>
+                <NavbarTop />
+                <App />
+                <NavbarBottom />
+            </BrowserRouter>
+            {/* </AuthGate> */}
+        </StrictMode>
+    )
+} else {
+    console.error("Root element not found.")
+}
