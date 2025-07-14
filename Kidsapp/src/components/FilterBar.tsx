db3c766cbdb0c3a2f1cd9454092f6e0095c2da3a
Lara <lara.koutsouraki002@stud.fh-dortmund.de>
2025-07-14 18:56:54 +0200

    fix: filterbar scalable

--- Kidsapp/src/components/FilterBar.tsx
+++ Kidsapp/src/components/FilterBar.tsx
@@ -1,9 +1,30 @@
+<<<<<<< Updated upstream:Kidsapp/src/components/FilterBar.tsx
 import {Box, TextField, InputAdornment, Autocomplete} from '@mui/material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import DatePicker from './DatePicker';
 import { Offer, OFFER_TYPE_OPTIONS } from '../models/AngebotType';
 import { useMemo, useState } from 'react';
 import LocationCityIcon from '@mui/icons-material/LocationCity';
+=======
+import { OFFER_TYPE_OPTIONS } from '../models/AngebotType';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import {
+  Box,
+  TextField,
+  InputAdornment,
+  Autocomplete,
+  IconButton,
+  MenuItem,
+  Paper,
+  Popper,
+  Typography
+} from '@mui/material';
+import { SearchOutlined, LocationCity, CalendarToday } from '@mui/icons-material';
+import { startOfWeek, endOfWeek } from 'date-fns';
+import { LocalizationProvider, ClearIcon, DatePicker } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { de } from 'date-fns/locale';
+>>>>>>> Stashed changes:src/components/FilterBar.tsx
 
 type CategoryOptions = typeof OFFER_TYPE_OPTIONS[number];
 
@@ -35,7 +56,45 @@ export default function FilterBar({
     return Array.from(citiesSet).sort((a, b) => a.localeCompare(b));
   }, [offers]);
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [openCustom, setOpenCustom] = useState(false);
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleOpen = () => {
+    setAnchorEl(inputRef.current);
+  };
+
+  const setToday = () => {
+    const today = new Date();
+    setStartDate(today);
+    setEndDate(today);
+    setAnchorEl(null);
+    setOpenCustom(false);
+  };
+
+  const setThisWeek = () => {
+    const today = new Date();
+    setStartDate(startOfWeek(today));
+    setEndDate(endOfWeek(today));
+    setAnchorEl(null);
+    setOpenCustom(false);
+  };
+
+  const toggleCustom = () => {
+    setOpenCustom(true);
+  };
+
+  useEffect(() => {
+    if (startDate && endDate) {
+      setAnchorEl(null);
+      setOpenCustom(false);
+    }
+  }, [startDate, endDate]);
+
   return (
+<<<<<<< Updated upstream:Kidsapp/src/components/FilterBar.tsx
     <Box
       sx={{
           p: { xs: 1, sm: 1, md: 1 },
@@ -51,28 +110,33 @@ export default function FilterBar({
           width: '95%',
           boxSizing: 'border-box',
       }}
+=======
+     <div
+      className={
+        "mx-auto sm:p-3 sm:w-11/12 lg:p-4 lg:w-4/5 max-w-screen-2xl flex flex-wrap items-center justify-center self-center w-[95%] p-2 gap-5 border border-gray-300 bg-white rounded-[20px]"
+      }
+>>>>>>> Stashed changes:src/components/FilterBar.tsx
     >
+      {/* Suche */}
       <TextField
-      variant="outlined"
-      label="Suche"
-      value={searchText}
-      onChange={(e) => setSearchText(e.target.value)}
-      slotProps={{
-        input: {
-          endAdornment: (
-            <InputAdornment position="end">
-              <SearchOutlinedIcon />
-            </InputAdornment>
-          )
-        }
-      }}
-      sx={{ width: 300, marginLeft: 5,
-        '& .MuiOutlinedInput-root': {
-          borderRadius: '30px',
-        },
-      }}
+        variant="outlined"
+        label="Suche"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        className="w-full sm:w-1/3 lg:w-1/5"
+        slotProps={{
+          input: {
+            endAdornment: (
+              <InputAdornment position="end">
+                <SearchOutlined />
+              </InputAdornment>
+            )
+          }
+        }}
+        sx={{ '& .MuiOutlinedInput-root': { borderRadius: '15px' } }}
       />
       
+      {/* Stadt */}
       <Autocomplete
         freeSolo
         options={cityOptions}
@@ -81,32 +145,28 @@ export default function FilterBar({
           setCityInputValue(newInput);
             setUserCity(newInput.trim() || null);
         }}
+        className="w-full sm:w-1/3 lg:w-1/5"
         renderInput={(params) => (
           <TextField
-      {...params}
-      label="Stadt"
-      InputProps={{
-        ...params.InputProps,
-        endAdornment: (
-          <>
-            {params.InputProps.endAdornment}
-            <InputAdornment position="end">
-              <LocationCityIcon />
-            </InputAdornment>
-          </>
-        ),
-      }}
-    />
-        )}
-        sx={{
-          width: 300,
-          '& .MuiOutlinedInput-root': {
-            borderRadius: '15px',
-        
-          },
-        }}
+            {...params}
+            label="Stadt"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {params.InputProps.endAdornment}
+                  <InputAdornment position="end">
+                    <LocationCity />
+                  </InputAdornment>
+                </>
+              ),
+            }}
+            sx={{ '& .MuiOutlinedInput-root': { borderRadius: '15px' } }}
+          />
+        )} 
       />
 
+      {/* Kategorie */}
       <Autocomplete<CategoryOptions>
         disablePortal
         options={OFFER_TYPE_OPTIONS}
@@ -119,15 +179,89 @@ export default function FilterBar({
         onChange={(_, newValue) => {
           setUserCategory(newValue ? newValue.value : null);
         }}
-        renderInput={(params) => <TextField {...params} label="Kategorie" />}
-        sx={{ width: 300,
-          '& .MuiOutlinedInput-root': {
-            borderRadius: '15px',
-          },
-        }}
+        className="w-full sm:w-1/3 lg:w-1/5"
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Kategorie"
+            sx={{ '& .MuiOutlinedInput-root': { borderRadius: "15px"} }}
+          />
+        )}
       />
 
-      <DatePicker />
-    </Box>
+      {/* Datum */}
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={de}>
+      <TextField
+        label="Datum"
+        value={
+          startDate && endDate
+            ? `${startDate.toLocaleDateString()} - ${endDate.toLocaleDateString()}`
+            : ''
+        }
+        className="w-full sm:w-1/3 lg:w-1/5"
+        slotProps={{
+            input: {
+              ref: inputRef,
+              onClick: handleOpen,
+              readOnly: true,
+              endAdornment: (
+                <InputAdornment position="end" sx={{ display: 'flex', gap: 1 }}>
+                  {startDate && endDate && (
+                    <IconButton
+                      size="small"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setStartDate(null);
+                        setEndDate(null);
+                      }}
+                    >
+                      <ClearIcon />
+                    </IconButton>
+                  )}
+                  <CalendarToday />
+                </InputAdornment>
+              ),
+            },
+          }}
+        sx={{ '& .MuiOutlinedInput-root': { borderRadius: '15px' } }}
+      />
+
+      <Popper open={!!anchorEl} anchorEl={anchorEl} placement="bottom-start">
+        <Paper sx={{ p: 2, minWidth: 250 }}>
+          <MenuItem onClick={setToday}>Heute</MenuItem>
+          <MenuItem onClick={setThisWeek}>Diese Woche</MenuItem>
+          <MenuItem onClick={toggleCustom}>
+            <Typography variant="body1">Genauer Zeitraum</Typography>
+          </MenuItem>
+          
+          {openCustom && (
+            <Box mt={2} display="flex" flexDirection="column" gap={0.5}>
+              <DatePicker
+                label="Startdatum"
+                value={startDate}
+                onChange={(newValue) => setStartDate(newValue)}
+                slotProps={{
+                  textField: {
+                    size: 'small',
+                    
+                  },
+                }}
+              />
+              <DatePicker
+                label="Enddatum"
+                value={endDate}
+                onChange={(newValue) => setEndDate(newValue)}
+                slotProps={{
+                  textField: {
+                    size: 'small',
+                  },
+                }}
+              />
+            </Box>
+          )}
+        </Paper>
+      </Popper>
+    </LocalizationProvider>
+    </div>
   );
 }
